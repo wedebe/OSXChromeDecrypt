9594f58f78d07691287b41e85339ea8cb487677e
Web Dev Ben <9741693+wedebe@users.noreply.github.com>
2022-03-16 12:19:36 +0000

    Update for Python3; Output CSV format
    This change:
    - updates to Python3 syntax
    - outputs CSV format to allow Chrome/Brave `Import`
    
    TO save to file, append `> outputfile.csv`
    
    *! REMEMBER TO DELETE THE SAVED FILE WHEN DONE !*

--- ChromePasswords.py
+++ ChromePasswords.py
@@ -1,20 +1,31 @@
 import sqlite3, os, binascii, subprocess, base64, sys, hashlib, glob
- 
+from urllib.parse import urlparse
+
+
 loginData = glob.glob("%s/Library/Application Support/Google/Chrome/Profile*/Login Data" % os.path.expanduser("~"))
+
 if len(loginData) == 0:
     loginData = glob.glob("%s/Library/Application Support/Google/Chrome/Default/Login Data" % os.path.expanduser("~")) #attempt default profile
+
 safeStorageKey = subprocess.check_output("security 2>&1 > /dev/null find-generic-password -ga 'Chrome' | awk '{print $2}'", shell=True).replace("\n", "").replace("\"", "")
+
 if safeStorageKey == "":
-    print "ERROR getting Chrome Safe Storage Key"
+    print("ERROR getting Chrome Safe Storage Key")
+    #TODO: prompt user to "Copy Password to Clipboard" from MacOS' Keychain Access utility
+    #(login-keychain.db from a different machine can be imported using Import Items, but RENAME IT FIRST!)
     sys.exit()
 
 def chromeDecrypt(encrypted_value, iv, key=None): #AES decryption using the PBKDF2 key and 16x ' ' IV, via openSSL (installed on OSX natively)
-    hexKey = binascii.hexlify(key)
-    hexEncPassword = base64.b64encode(encrypted_value[3:])
+    hexKey = bytes.hex(key)
+    message_bytes = encrypted_value[3:]
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    hexEncPassword = base64_message
+
     try: #send any error messages to /dev/null to prevent screen bloating up
-        decrypted = subprocess.check_output("openssl enc -base64 -d -aes-128-cbc -iv '%s' -K %s <<< %s 2>/dev/null" % (iv, hexKey, hexEncPassword), shell=True)
+        decrypted = subprocess.check_output("openssl enc -base64 -d -aes-128-cbc -iv '%s' -K %s <<< %s 2>/dev/null" % (iv, hexKey, hexEncPassword), shell=True).decode('ascii')
     except Exception as e:
-        decrypted = "ERROR retrieving password"
+        decrypted = ("ERROR retrieving password: %s" % e)
     return decrypted
 
 def chromeProcess(safeStorageKey, loginData):
@@ -30,10 +41,11 @@ def chromeProcess(safeStorageKey, loginData):
             if user == "" or (encryptedPass[:3] != b'v10'): #user will be empty if they have selected "never" store password
                 continue
             else:
-                urlUserPassDecrypted = (url.encode('ascii', 'ignore'), user.encode('ascii', 'ignore'), chromeDecrypt(encryptedPass, iv, key=key).encode('ascii', 'ignore'))
+                urlUserPassDecrypted = (url, user, chromeDecrypt(encryptedPass, iv, key=key))
                 decryptedList.append(urlUserPassDecrypted)
     return decryptedList
 
 for profile in loginData:
+    print('name,url,username,password')
     for i, x in enumerate(chromeProcess(safeStorageKey, "%s" % profile)):
-    	print "%s[%s]%s %s%s%s\n\t%sUser%s: %s\n\t%sPass%s: %s" % ("\033[32m", (i + 1), "\033[0m", "\033[1m", x[0], "\033[0m", "\033[32m", "\033[0m", x[1], "\033[32m", "\033[0m", x[2])
+    	print("%s,%s,%s,%s" % (urlparse(x[0]).netloc, x[0], x[1], x[2]))
